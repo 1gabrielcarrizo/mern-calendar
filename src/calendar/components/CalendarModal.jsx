57b809fbb8d639de193c6401e49e66ed4d4dc969
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2024-03-28 18:27:46 -0300

    datepicker in spanish

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,5 +1,11 @@
+import { addHours } from 'date-fns';
 import { useState } from 'react';
-import Modal from 'react-modal'
+import Modal from 'react-modal';
+import DatePicker, {registerLocale} from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import es from 'date-fns/locale/es'
+
+registerLocale('es', es)
 
 const customStyles = {
     content: {
@@ -17,6 +23,28 @@ Modal.setAppElement('#root'); // se sobrepone en los elementos
 export const CalendarModal = () => {
 
     const [isOpen, setIsOpen] = useState(true)
+    // 1- creamos un form de manera tradicional
+    const [formValues, setFormValues] = useState({
+        title: 'Gabriel',
+        notes: 'Carrizo',
+        start: new Date(),
+        end: addHours(new Date(), 2)
+    })
+
+    // 3- creamos una funcion para cambiar el form
+    const onInputChanged = ({ target }) => {
+        setFormValues({
+            ...formValues,
+            [target.name]: target.value
+        })
+    }
+
+    const onDateChanged = (event, changing) => {
+        setFormValues({
+            ...formValues,
+            [changing]: event
+        })
+    }
 
     const onCloseModal = () => {
         console.log("cerrando modal")
@@ -32,9 +60,74 @@ export const CalendarModal = () => {
             overlayClassName='modal-fondo'
             closeTimeoutMS={200}
         >
-            <h1>Hola mundo</h1>
+            <h1> Nuevo evento </h1>
             <hr />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quas alias ratione enim labore dolorum necessitatibus quos deserunt. Nihil perferendis voluptates, quisquam dicta nobis debitis fugit porro commodi dolores unde?</p>
+            <form className="container">
+
+                <div className="form-group mb-2">
+                    <label>Fecha y hora inicio</label>
+                    <DatePicker
+                        selected={formValues.start}
+                        onChange={(event) => onDateChanged(event, "start")}
+                        className='form-control'
+                        dateFormat='Pp'
+                        showTimeSelect
+                        locale='es'
+                        timeCaption='Hora'
+                    />
+                </div>
+
+                <div className="form-group mb-2">
+                    <label>Fecha y hora fin</label>
+                    <DatePicker
+                        minDate={formValues.start} // no seleciona flechas anterior a lo que dije
+                        selected={formValues.end}
+                        onChange={(event) => onDateChanged(event, "end")}
+                        className='form-control'
+                        dateFormat='Pp'
+                        showTimeSelect
+                        locale='es'
+                        timeCaption='Hora'
+                    />
+                </div>
+
+                <hr />
+                <div className="form-group mb-2">
+                    <label>Titulo y notas</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Título del evento"
+                        name="title"
+                        autoComplete="off"
+                        value={formValues.title} // 2- agregamos el value
+                        onChange={onInputChanged} // 4- llamamos a la funcion
+                    />
+                    <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+                </div>
+
+                <div className="form-group mb-2">
+                    <textarea
+                        type="text"
+                        className="form-control"
+                        placeholder="Notas"
+                        rows="5"
+                        name="notes"
+                        value={formValues.notes} // 2- agregamos el value
+                        onChange={onInputChanged} // 4- llamamos a la funcion
+                    ></textarea>
+                    <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+                </div>
+
+                <button
+                    type="submit"
+                    className="btn btn-outline-primary btn-block"
+                >
+                    <i className="far fa-save"></i>
+                    <span> Guardar</span>
+                </button>
+
+            </form>
         </Modal>
     )
 }
